25f2f8de832c817ddea9fdcd1727e16ddca757ae
Asif Ali <Asifali2735@gmail.com>
2026-01-08 15:33:14 +0000

    "use client";
    export default function Test() {
      return (
        <button

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Test() {
+  return (
+    <button
+      onClick={() => alert("Button is working")}
+      style={{ padding: "20px", background: "green", color: "white" }}
+    >
+      TEST BUTTON
+    </button>
+  );
+}
